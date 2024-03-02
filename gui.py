3d952a0b06adb880591fe92c6794955b32afe7fb
danutbradescu <dumitru.bradescu01@e-uvt.ro>
2024-03-02 21:53:00 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -11,16 +11,13 @@ root.title("Web Scraping Application")
 # Custom font
 customFont = font.Font(family="Helvetica", size=12)
 
-# ScrolledText widget for logging
 log_text = scrolledtext.ScrolledText(root, font=customFont, height=10, width=50)
 log_text.pack(pady=(10, 20))
 
-# Progress Bar (initially hidden)
 progress_bar = ttk.Progressbar(root, orient="horizontal", length=200, mode="indeterminate")
 progress_bar.pack(pady=(10, 0))
 progress_bar.pack_forget()  # Hide the progress bar initially
 
-# Status Label
 status_label = ttk.Label(root, text="", font=("Helvetica", 10))
 status_label.pack(pady=(5, 20))
 
@@ -28,10 +25,10 @@ def select_file():
     filepath = askopenfilename(filetypes=[("Parquet Files", "*.parquet"), ("All Files", "*.*")])
     if filepath:
         log_to_widget(f"Selected file: {filepath}")
-        select_file_button.pack_forget()  # Hide the "Select File" button
-        progress_bar.pack(pady=(10, 0))  # Show the progress bar
-        progress_bar.start(10)  # Start the progress bar when file is selected
-        status_label.config(text="Please wait...")  # Show the status message
+        select_file_button.pack_forget() 
+        progress_bar.pack(pady=(10, 0))  
+        progress_bar.start(10) 
+        status_label.config(text="Please wait...") 
         start_scraping(filepath)
 
 def start_scraping(filepath):
@@ -50,11 +47,9 @@ def log_to_widget(message):
     log_text.insert("end", message + "\n")
     log_text.see("end")
 
-# Apply the theme
 style = ttk.Style()
 style.theme_use('vista')
 
-# "Select File" Button
 select_file_button = Button(root, text="Select File", command=select_file, font=customFont)
 select_file_button.pack(pady=(20, 10))
 
